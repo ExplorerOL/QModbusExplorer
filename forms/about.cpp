fde053c9764dc541a1b909c4186f9cccf4a6c14e
ElBar <ElBar@localhost>
2018-09-01 20:06:41 +0300

    Small fixes.  Fix version 0.5.1-2.

--- forms/about.cpp
+++ forms/about.cpp
@@ -2,7 +2,7 @@
 #include "ui_about.h"
 #include "modbus-version.h"
 
-const QString VER = "QModMaster 0.5.1-1";
+const QString VER = "QModMaster 0.5.1-2";
 const QString LIB_VER = LIBMODBUS_VERSION_STRING;
 const QString URL = "<a href = ""http://sourceforge.net/projects/qmodmaster"">Sourceforge Project Home Page</a>";
 
