32b554865795f8eb6e9bd0cb64df8ed3e01c12c6
ExplorerOL <dmitry.vorobjev.mailbox@yandex.ru>
2022-04-03 12:32:38 +0300

    Change valid address from 0 to 255 (QModMaster commit)

--- 3rdparty/libmodbus/modbus-rtu.c
+++ 3rdparty/libmodbus/modbus-rtu.c
@@ -92,7 +92,8 @@ static const uint8_t table_crc_lo[] = {
 static int _modbus_set_slave(modbus_t *ctx, int slave)
 {
     /* Broadcast address is 0 (MODBUS_BROADCAST_ADDRESS) */
-    if (slave >= 0 && slave <= 247) {
+    //***changed for some devices : <= 247***//
+    if (slave >= 0 && slave <= 255) {
         ctx->slave = slave;
     } else {
         errno = EINVAL;
